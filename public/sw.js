9f6deffc4bca132541040a8ce84c515ed2d17596
Alsoufi0 <ahmedalsoufi44@gmail.com>
2026-04-28 02:58:53 +0300

    Bump cache for visual study rendering

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'simav-dental-tutor-v6';
+const CACHE_NAME = 'simav-dental-tutor-v7';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
