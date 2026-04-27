a45bc1789cc4875259b15bfa1e89a73d23c9b090
Alsoufi0 <ahmedalsoufi44@gmail.com>
2026-04-28 01:19:56 +0300

    Bump service worker cache for upload fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'simav-dental-tutor-v1';
+const CACHE_NAME = 'simav-dental-tutor-v2';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
