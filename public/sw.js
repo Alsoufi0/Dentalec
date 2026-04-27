f1b83079896a7bce54b8803f22a0c0d9e60967c7
Alsoufi0 <ahmedalsoufi44@gmail.com>
2026-04-28 01:31:53 +0300

    Refresh cache for dashboard polish

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'simav-dental-tutor-v2';
+const CACHE_NAME = 'simav-dental-tutor-v3';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
