75ed743a234be72dbbbe892939094bcf3e21f702
Alsoufi0 <ahmedalsoufi44@gmail.com>
2026-04-28 06:28:06 +0300

    Bump cache for logo and efficiency update

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'simav-dental-tutor-v8';
-const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon.svg'];
+const CACHE_NAME = 'simav-dental-tutor-v9';
+const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon.svg', '/brand.css'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
